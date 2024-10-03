1549c82c35b3449aa0f4886e96060de559ae8447
Luis Henrique <luish7572@gmail.com>
2024-10-03 20:51:00 -0300

    programando com o ajax

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#nome');
+    const usernameElement = document.querySelector('#usuario');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repositorios');
+    const followersElement = document.querySelector('#seguidores');
+    const followingElement = document.querySelector('#seguindo');
+    const linkElement = document.querySelector('#link');
+
+})
