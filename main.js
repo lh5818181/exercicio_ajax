39a4c872a9a2a8a9adbaac79660b33c6bb09d5e4
Luis Henrique <lh5818181@gmail.com>
2024-10-05 17:03:28 -0300

    ajustando ajax

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const nameElement = document.querySelector('#nome');
     const usernameElement = document.querySelector('#usuario');
     const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repositorios');
+    const reposElement = document.querySelector('#repositorio');
     const followersElement = document.querySelector('#seguidores');
     const followingElement = document.querySelector('#seguindo');
     const linkElement = document.querySelector('#link');
 
+
+    fetch('https://api.github.com/users/lh5818181')
+    .then(function(res) {
+        return res.json();
+
+    })
+    .then(function(json) {
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        followingElement.innerText = json.following;
+        followersElement.innerText = json.followers;
+        repos.innerText = json.public_repos;
+        linkElement.href = json.html_url;
+    })
 })
